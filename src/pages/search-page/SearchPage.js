4253e0019b191a0e989e0c76b9e15c82504001d9
Kaeneth Dayao <kendayao@gmail.com>
2021-01-11 12:31:50 -0800

    styled search page header

--- src/pages/search-page/SearchPage.js
+++ src/pages/search-page/SearchPage.js
@@ -4,6 +4,13 @@ import useGoogelSearch from '../../useGoogelSearch';
 import './SearchPage.css'
 import responseData from '../../response'
 import {Link} from 'react-router-dom'
+import Search from '../../components/search/Search'
+import SearchIcon from '@material-ui/icons/Search';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ImageIcon from '@material-ui/icons/Image';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import RoomIcon from '@material-ui/icons/Room';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function SearchPage() {
     const[{term},dispatch]=useStateValue();
@@ -18,6 +25,45 @@ function SearchPage() {
                     <img className='searchPage__logo'
                     src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' alt='google logo'/>
                 </Link>
+                <div className='searchPage__headerBody'>
+                    <Search hideButttons/>
+                    <div className='searchPage__options'>
+                        <div className='searchPage__optionsLeft'>
+                            <div className='searchPage__option'>
+                                <SearchIcon />
+                                <Link to='all'>All</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <DescriptionIcon />
+                                <Link to='/news'>News</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <ImageIcon />
+                                <Link to='/images'>Images</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <LocalOfferIcon />
+                                <Link to='/shopping'>shopping</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <RoomIcon />
+                                <Link to='/maps'>maps</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <MoreVertIcon />
+                                <Link to='/more'>more</Link>
+                            </div>
+                        </div>
+                        <div className='searchPage__optionsRight'>
+                            <div className='searchPage__option'>
+                                <Link to='/settings'>Settings</Link>
+                            </div>
+                            <div className='searchPage__option'>
+                                <Link to='/tools'>Tools</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className='searchPage__results'>
 
