aa40a0c8fa13d55ed7f156b37dd166c04fa6fb60
Kaeneth Dayao <kendayao@gmail.com>
2021-01-11 09:31:20 -0800

    styled search ocmponent

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Search.css'
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import {Button} from '@material-ui/core'
+import { useHistory } from 'react-router-dom';
 
-function Search() {
+function Search({hideButttons}) {
+
+    const[input, setInput]=useState('');
+    const history=useHistory();
+
+    const search=e=>{
+        e.preventDefault();
+
+        history.push('/search')
+    };
+
     return (
-        <div className='search'>
+        <form className='search'>
             <div className='search__input'>
                 <SearchIcon className='search__inputIcon'/>
-                <input />
+                <input value={input} onChange={e=>setInput(e.target.value)} />
                 <MicIcon />
             </div>
-            <div className='search__buttons'>
-                <Button variant='outlined'>Google Search</Button>
-                <Button variant='outlined'>I'm Feeling Lucky</Button>
-            </div>
-        </div>
+            {!hideButttons? 
+             (<div className='search__buttons'>
+             <Button type='submit' onClick={search} variant='outlined'>Google Search</Button>
+             <Button variant='outlined'>I'm Feeling Lucky</Button>
+            </div>):
+            (<div className='search__buttons'>
+            <Button className='search__buttonsHidden' type='submit' onClick={search} variant='outlined'>Google Search</Button>
+            <Button className='search__buttonsHidden' variant='outlined'>I'm Feeling Lucky</Button>
+            </div>)} 
+        </form>
     )
 }
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -23,7 +23,7 @@ function Home() {
             <div className='home__body'>
                 <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="google logo"/>
                 <div className="home__inputContainer">
-                    <Search />
+                    <Search hideButttons />
                 </div>
             </div>
         </div>
