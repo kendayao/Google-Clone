ab210084ab622da194a80b4f90c77ae04251f888
Kaeneth Dayao <kendayao@gmail.com>
2021-01-20 13:01:37 -0800

    added sidebar for email

--- src/components/mail-header-side-row/MailHeaderSideRow.js
+++ src/components/mail-header-side-row/MailHeaderSideRow.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './MailHeaderSideRow.css'
 
-function MailHeaderSideRow() {
+
+
+function MailHeaderSideRow({Icon, title, active}) {
     return (
-        <div className='mailHeaderSideRow'>
-            
+        <div className={active?'mailHeaderSideRow mailHeaderSideRow--active': 'mailHeaderSideRow'}>
+            <div className='mailHeaderSideRow__left'>
+            <Icon fontSize='small' className={active?'mailHeaderSideRow--active': 'mailHeaderSideRow__icon'} />
+            <p>{title}</p>
+            </div>
+            <div className={active?'mailHeaderSideRow__right--show mailHeaderSideRow__right':'mailHeaderSideRow__right--hide'}>
+                <p>100</p>
+            </div>         
         </div>
     )
 }
--- src/components/mail-header-side/MailHeaderSide.js
+++ src/components/mail-header-side/MailHeaderSide.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import './MailHeaderSide.css'
-import AddOutlinedIcon from '@material-ui/icons/AddOutlined';
+import MailHeaderSideRow from '../mail-header-side-row/MailHeaderSideRow'
+import InboxIcon from '@material-ui/icons/Inbox';
+import StarIcon from '@material-ui/icons/Star';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import SendIcon from '@material-ui/icons/Send';
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import LabelIcon from '@material-ui/icons/Label';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import KeyboardIcon from '@material-ui/icons/Keyboard';
 
 function MailHeaderSide() {
     return (
         <div className='mailheaderside'>
             <button><img src='https://www.gstatic.com/images/icons/material/colored_icons/1x/create_32dp.png' className='mailheaderside__compose-img'/>Compose</button>
             <div className='mailheaderside__options'>
-
+                <MailHeaderSideRow Icon={InboxIcon} title={'Inbox'} active />
+                <MailHeaderSideRow Icon={StarIcon} title={'Starred'} />
+                <MailHeaderSideRow Icon={WatchLaterIcon} title={'Snoozed'} />
+                <MailHeaderSideRow Icon={LabelImportantIcon} title={'Important'} />
+                <MailHeaderSideRow Icon={SendIcon} title={'Sent'} />
+                <MailHeaderSideRow Icon={InsertDriveFileIcon} title={'Drafts'} />
+                <MailHeaderSideRow Icon={LabelIcon} title={'Personal'} />
+                <MailHeaderSideRow Icon={KeyboardArrowDownIcon} title={'More'} />
             </div>
+            <div className='mailheaderside__meet-options'>
+                <p>Meet</p>
+                <MailHeaderSideRow Icon={VideocamIcon} title={'New meeting'} />
+                <MailHeaderSideRow Icon={KeyboardIcon} title={'Join a meeting'} />
+            </div>
+
         </div>
     )
 }
